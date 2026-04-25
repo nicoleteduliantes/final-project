42999bb3e050d2a98d69364c387a6cdb21149d76
Ezra-Ysabela-Gellecania <ezraysabelagellecania@gmail.com>
2026-04-25 11:52:50 +0800

    Made frontend ready for backend connection

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,4 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
+import { useAuthStore } from '@/stores/auth';
 
-import App from './App.vue';
-import router from './router';
-
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount('#app');
+const auth = useAuthStore();
+auth.loadUser();
