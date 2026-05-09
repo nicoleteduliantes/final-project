d4ae84f08bf34bc89a9f1e589a3403a3f1c749b2
Nicole Marie Eduliantes <nicoleteduliantes@gmail.com>
2026-05-10 00:27:18 +0800

    Set up announcement model and migrations

---
+++ app/Models/Announcement.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Announcement extends Model
+{
+    protected $primaryKey = 'announcement_id';
+    protected $fillable = ['title', 'content', 'org_id', 'osa_id'];
+
+    public function organization() {
+        return $this->belongsTo(Organization::class, 'org_id');
+    }
+
+    public function osa() {
+        return $this->belongsTo(OSA::class, 'osa_id');
+    }
+}
